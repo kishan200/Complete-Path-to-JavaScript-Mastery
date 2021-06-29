a484d843ec44e0ca37ede56182312b2a7819fe77
kishan200 <sarvaiyakishank@gmail.com>
2021-06-29 23:19:43 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,39 @@
+// CREATING A VARIABLE USING VAR KEYWORD
+// var name = "kishan";
+
+// CREATING A VARIABLE USING VAR KEYWORD
+// let name = "kishan";
+
+// CREATING A VARIABLE USING VAR KEYWORD
+//const name = "kishan";
+
+// *** String ***
+const singQuotes = "hello !";
+const doubleQuetes = "hello!!";
+const name = "kishan";
+const backtick = `Hello, ${name}! , welcome`;
+const backticks = `${2 + 2}`;
+
+//*** NUmber ***
+const WholeNo = 5;
+const decimalno = 0.5;
+
+const fno = 5;
+const sno = 10;
+const result = fno + sno;
+
+//***Booleasn ***
+// true - yes ,correct,1
+//false -no,incorrect,0
+
+const isCool = true;
+
+if (isCool) {
+  console.log("hi man ,you're cool");
+} else {
+  console.log("oh,hi...");
+}
+
+const age = 21;
+
+console.log(age < 18);

172c4105dfe852b477e60797f45ebdda4bebc99c
kishan200 <sarvaiyakishank@gmail.com>
2021-06-30 22:08:10 +0530

    assigment operatot

--- Script.js
+++ Script.js
@@ -8,32 +8,149 @@
 //const name = "kishan";
 
 // *** String ***
-const singQuotes = "hello !";
-const doubleQuetes = "hello!!";
-const name = "kishan";
-const backtick = `Hello, ${name}! , welcome`;
-const backticks = `${2 + 2}`;
+// const singQuotes = "hello !";
+// const doubleQuetes = "hello!!";
+// const name = "kishan";
+// const backtick = `Hello, ${name}! , welcome`;
+// const backticks = `${2 + 2}`;
 
 //*** NUmber ***
-const WholeNo = 5;
-const decimalno = 0.5;
+// const WholeNo = 5;
+// const decimalno = 0.5;
 
-const fno = 5;
-const sno = 10;
-const result = fno + sno;
+// const fno = 5;
+// const sno = 10;
+// const result = fno + sno;
 
 //***Booleasn ***
 // true - yes ,correct,1
 //false -no,incorrect,0
 
-const isCool = true;
+// const isCool = true;
 
-if (isCool) {
-  console.log("hi man ,you're cool");
+// if (isCool) {
+//   console.log("hi man ,you're cool");
+// } else {
+//   console.log("oh,hi...");
+// }
+
+//const age = 21;
+
+//null
+//let age = null;
+
+//undefine
+//let x;
+
+//object
+// const person = {
+//   name: "kishan",
+//   age: 25,
+// };
+
+// Array
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+
+// const data = new Date();
+// console.log(data);
+
+// arithmetic opration
+// const a = 5;
+// const b = 10;
+// let result = 0;
+
+// //addition
+//     result = a + b;
+// //sub
+//     result = a - b;
+// //mul
+//     result = a * b;
+// //div
+//     result = a / b;
+//exponent
+//result = b ** a;
+
+//modulo
+//12%13
+//result = a % b;
+
+//incremenr decrement
+
+//result++;
+//result--;
+
+//**** comarison operators  */
+const a = 5;
+const b = 10;
+
+// console.log(a > b);
+
+// console.log(a < b);
+
+// //is equal
+// console.log(a == b);
+
+//not equal
+//console.log(a !== b);
+
+//***** Logical operation */
+
+//and && ==> All opwrand are TRUE => TRUE
+//console.log(true && false && true);
+
+//or || => at list one operand is true =>true
+//console.log(true || false);
+
+//not !
+//console.log(!true);
+
+//***assignmennt operator* */
+
+// let no = 5;
+
+// no += 5; //no = no + 5'
+// no -= 5; //no = no - 5'
+// no *= 5; //no = no * 5'
+// no /= 5; //no = no / 5'
+
+// let string = "hello";
+
+// string += ", I am ksiahn!";
+//console.log(string);
+
+//***** if statement */
+
+// const age = 18;
+
+// if (age > 18) {
+//   console.log("you may enter");
+// } else if (age == 18) {
+//   console.log("you just turnet 18");
+// } else {
+//   console.log("grow up");
+// }
+
+//***** truthy -falsy-value */
+
+const dog = 5;
+if (dog) {
+  console.log(`you have ${dog} dog `);
 } else {
-  console.log("oh,hi...");
+  console.log("you have no dog");
 }
 
-const age = 21;
+//falsy values => false
+// false
+// 0
+// ''
+// null
+// undefined
+// NaN
 
-console.log(age < 18);
+// true
+// 1
+// "hello"
+// 85
+// {}
+// []

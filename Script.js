8082611a93116c5211626bff1c377e9b324b7a1f
kishan200 <sarvaiyakishank@gmail.com>
2021-07-06 10:21:53 +0530

    array

--- Script.js
+++ Script.js
@@ -133,12 +133,12 @@ const b = 10;
 
 //***** truthy -falsy-value */
 
-const dog = 5;
-if (dog) {
-  console.log(`you have ${dog} dog `);
-} else {
-  console.log("you have no dog");
-}
+// const dog = 5;
+// if (dog) {
+//   console.log(`you have ${dog} dog `);
+// } else {
+//   console.log("you have no dog");
+// }
 
 //falsy values => false
 // false
@@ -154,3 +154,274 @@ if (dog) {
 // 85
 // {}
 // []
+
+//*** Logical operation */
+
+// const age = 18;
+// const isCool = true;
+// if (isCool && age > 18) {
+//   console.log("you my enter !");
+// } else {
+//   console.log("you cannot enter");
+// }
+
+// console.log("truyh" && "test" && 999);
+
+//console.log("truyh" || 1 || "test" || 999);
+
+//console.log(!0);
+
+// const value = false;
+// if (!!value) {
+//   console.log("value is Truthy");
+// } else {
+//   console.log("value is falsy");
+// }
+
+/******* switch statement */
+
+//const superhero = "iron man";
+
+// switch (superhero) {
+//   case "iron man":
+//     console.log("i am iron man...");
+
+//     break;
+
+//   case "thor":
+//     console.log("this is my hammer");
+//     break;
+
+//   default:
+//     console.log("enter valid super hero");
+// }
+
+//28.********* ternary operator
+
+//condition ? true : false;
+
+//
+
+//*****29.looping while and for -loops */
+// the while loop
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+//* for loop
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+//***** function */
+
+// A  FUNCTION DECLARATION
+// function square(x) {
+//   return x * x;
+// }
+// //A FUNCTION CALL
+// const result = square(5);
+// console.log(result);
+
+// // A FUNCTION DECLARATION
+// function name(params){
+//     //ststements
+//  }
+// //A FUNCTION EXPRESSION
+
+//     const name = function(params){
+//         //statement
+//     }
+//     //an ARROW FUNCTION/
+//    const name  = (params) => {
+//        //statement
+//    }
+
+// function sayHi(name) {
+//   console.log(`hi,${name}`);
+// }
+// sayHi("kishan");
+// sayHi("google");
+
+//RETURNS INDEFINED
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// const sum = add(20, 5);
+
+// console.log(sum);
+
+// function test() {
+//   console.log(1);
+//   return true;
+//   console.log(2);
+//   return false;
+// }
+
+// const bool = test();
+
+// console.log(bool);
+
+//***33  arrow finction */
+
+// const square = (no) => {
+//   return no * no;
+// };
+
+// const square1 = (no) => no * no;
+
+// const result = square(5);
+// const result1 = square(10);
+
+// console.log(result);
+
+// console.log(result1);
+
+//paramaters -used when defining a func
+//arrguments - passed when making a func call
+
+// const sayHi = (name, age) => {
+//   console.log(`${name} is ${age} year old`);
+// };
+// sayHi("kishan", 21);
+
+//**** 36 GLOBAL SCOPE */
+
+// const name = "kishan";
+
+// const logName = () => {
+//   console.log(name);
+// };
+// logName();
+
+// local scope / function scope
+// const someFunction = () => {
+//   //local scope
+//   const name = "janny";
+//   console.log(name);
+// };
+// const anotherFunction = () => {
+//   //local scope
+//   const name = "kishan";
+//   console.log(name);
+// };
+// anotherFunction();
+// someFunction();
+
+////**block scope */
+
+// if (true) {
+//   const name = "kishan";
+//   console.log(name);
+// }
+
+// console.log(name);
+
+//***37 hosting in javascript */
+// console.log(aeg);
+
+// var age = 20;
+
+// function hoist() {
+//   var msg = "test";
+//   console.log(msg);
+//   }
+// hoist();
+
+//**** 38. closure */
+
+// const outer = () => {
+//   const outerVar = "hello";
+
+//   const inner = () => {
+//     const innerVar = "hi";
+
+//     console.log(outerVar, innerVar);
+//   };
+//   return inner;
+// };
+// const innerFn = outer();
+
+// innerFn();
+
+// const init = () => {
+//   const hobby = "learning js"; // Local variable created by inner function
+
+//   const displayHobby = () => {
+//     //display is the inner function a closure
+//     console.log(hobby); // using a variable created in the parent function
+//   };
+//   displayHobby();
+// };
+// init();
+
+//***48 array  */
+
+// const month = ["january", "february", "march", "april"];
+// //console.log(month);
+// for (let i = 0; i < month.length; i++) {
+//   console.log(month[i]);
+// }
+
+// //****49 array method */
+
+// const names = ["kishan", "akash", "jigar", "mars"];
+// //arry push -add new element
+// names.push("rahul");
+
+// //arry pop- delete the last element of array
+// names.pop();
+
+// //array shift delete the first elements of an arry
+// names.shift();
+
+// // arry unshift add a new value of yhe start of an array
+// names.unshift("hitesh");
+
+// //arry splice- to add/remove new valuew in ant position
+// //names.splice(2, 0, "harshil", "shubham");
+
+// //array slice-copies certain of an array into a neely created arry
+// //const noonLink = names.slice(2);
+
+// console.log(names);
+
+//*****50 Array forEach*/
+
+//***51 Array map
+const name = ["kishan", "rahul", "akash", "jigar"];
+//array foeEach
+
+name.forEach((name, i) => {
+  console.log(name, i);
+});
+
+let sum = 0;
+const numbers = [10, 50, 9, 81];
+
+numbers.forEach((number) => {
+  sum += number;
+});
+console.log(sum);
+
+// const inventary = [
+//   { price: 5, name: "apple" },
+//   { price: 4, name: "orange" },
+//   { price: 3, name: "mango " },
+//   { price: 5, name: "banana" },
+// ];
+
+// //array map
+
+// const prices = inventary.map((item) => item.price);
+// const items = inventary.map((item) => item.name);
+
+// console.log(prices);
+
+// console.log(items);
